ee808648ebc50063846f6789894a591cc65d5f19
carika@virtualforce.co.za <nadir.alishah@weku.solutions>
2024-11-10 14:42:38 +0200

    reporting test field list fix

--- app/Console/Commands/DialStats.php
+++ app/Console/Commands/DialStats.php
@@ -31,19 +31,18 @@ class DialStats extends Command
     public function handle()
     {
         set_time_limit(0);
-        DB::update("insert into dial_stats
-        select DATE_FORMAT(dd.created_at, '%Y-%m-%d') as dispositiondate, campaignid, dd.instanceid, c.name, dispositionid, 
+        DB::update("insert into dial_stats (dispositiondate, campaignid, instanceid, campaign, dispositionid, disposition, callduration)
+        select DATE_FORMAT(dd.created_at, '%Y-%m-%d') as dispositiondate, campaignid, dd.instanceid, c.name as campaign, dispositionid, 
         disposition, sum(callduration) as callduration from dial_dispositions dd join campaigns c on dd.campaignid=c.id
         group by DATE_FORMAT(dd.created_at, '%Y-%m-%d'), campaignid, dd.instanceid, c.name, dispositionid, 
-        disposition
-        on duplicate key update 
-        dispositiondate=DATE_FORMAT(dd.created_at, '%Y-%m-%d'), 
-        campaignid=dd.campaignid,
-        instanceid=dd.instanceid,
-        campaign=c.name,
-        dispositionid=dd.dispositionid,
-        disposition=dd.disposition,
-        callduration=dd.callduration;");
+        disposition ON DUPLICATE KEY UPDATE 
+        dispositiondate=DATE_FORMAT(dispositiondate, '%Y-%m-%d'), 
+        campaignid=campaignid,
+        instanceid=instanceid,
+        campaign=campaign,
+        dispositionid=dispositionid,
+        disposition=disposition,
+        callduration=callduration;");
         return 0;
     }
 }
