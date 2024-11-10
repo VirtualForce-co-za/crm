4a2a12bd298cd49e595388ff3634f7ccd21584ce
carika@virtualforce.co.za <nadir.alishah@weku.solutions>
2024-11-10 21:03:31 +0200

    winning 1

--- app/Console/Commands/DialStats.php
+++ app/Console/Commands/DialStats.php
@@ -31,7 +31,9 @@ class DialStats extends Command
     public function handle()
     {
         set_time_limit(0);
-        DB::update("insert into dial_stats (dispositiondate, campaignid, instanceid, campaign, dispositionid, disposition, callduration)
+        DB::statement(' SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED');
+
+        DB::statement("insert into dial_stats (dispositiondate, campaignid, instanceid, campaign, dispositionid, disposition, callduration)
         select DATE_FORMAT(dd.created_at, '%Y-%m-%d') as dispositiondate, campaignid, dd.instanceid, c.name as campaign, dispositionid, 
         disposition, sum(callduration) as callduration from dial_dispositions dd join campaigns c on dd.campaignid=c.id
         group by DATE_FORMAT(dd.created_at, '%Y-%m-%d'), campaignid, dd.instanceid, c.name, dispositionid, 
@@ -44,32 +46,40 @@ class DialStats extends Command
         disposition=disposition,
         callduration=callduration;");
 
-        DB::update("insert into dial_stat_by_rows (dispositiondate, campaignid, instanceid, campaign, callduration, dial)
+        DB::statement("insert into dial_stat_by_rows (dispositiondate, campaignid, instanceid, campaign, callduration, dial)
+        SELECT * FROM (
         select DATE_FORMAT(dd.created_at, '%Y-%m-%d') as dispositiondate, campaignid, dd.instanceid, c.name as campaign, 
         sum(callduration) as callduration, 
         count(dd.id) as dial
         from dial_dispositions dd join campaigns c on dd.campaignid=c.id
         where DATE_FORMAT(dd.created_at, '%Y-%m-%d')=curdate()
-        group by DATE_FORMAT(dd.created_at, '%Y-%m-%d'), campaignid, instanceid, c.name ON DUPLICATE KEY UPDATE 
-        dispositiondate=DATE_FORMAT(dispositiondate, '%Y-%m-%d'), 
-        campaignid=campaignid,
-        instanceid=instanceid,
-        campaign=campaign,
-        callduration=callduration,
-        dial=dial;");
+        group by DATE_FORMAT(dd.created_at, '%Y-%m-%d'), campaignid, instanceid, c.name 
+        ) A
+        ON DUPLICATE KEY UPDATE 
+        dispositiondate=DATE_FORMAT(A.dispositiondate, '%Y-%m-%d'), 
+        campaignid=A.campaignid,
+        instanceid=A.instanceid,
+        campaign=A.campaign,
+        callduration=A.callduration,
+        dial=A.dial;");
 
-        DB::update("insert into dial_stat_by_rows (dispositiondate, campaignid, instanceid, campaign, connected, connected_duration)
+        DB::statement("insert into dial_stat_by_rows (dispositiondate, campaignid, instanceid, campaign, connected, connected_duration)
+        SELECT * FROM (
         select DATE_FORMAT(dd.created_at, '%Y-%m-%d') as dispositiondate, campaignid, dd.instanceid, c.name as campaign, 
         count(dd.id) as connected,
         sum(callduration) as connected_duration
         from dial_dispositions dd join campaigns c on dd.campaignid=c.id
-        group by DATE_FORMAT(dd.created_at, '%Y-%m-%d'), campaignid, instanceid, c.name ON DUPLICATE KEY UPDATE 
-        dispositiondate=DATE_FORMAT(dispositiondate, '%Y-%m-%d'), 
-        campaignid=campaignid,
-        instanceid=instanceid,
-        campaign=campaign,
-        connected=connected,
-        connected_duration=connected_duration;");
+        where dd.disposition not in ('No Answer', 'Busy', 'Failed', 'Voicemail', 'Silent')
+        and DATE_FORMAT(dd.created_at, '%Y-%m-%d')=curdate()
+        group by DATE_FORMAT(dd.created_at, '%Y-%m-%d'), campaignid, instanceid, c.name
+        ) A
+        ON DUPLICATE KEY UPDATE 
+        dispositiondate=DATE_FORMAT(A.dispositiondate, '%Y-%m-%d'), 
+        campaignid=A.campaignid,
+        instanceid=A.instanceid,
+        campaign=A.campaign,
+        connected=A.connected,
+        connected_duration=A.connected_duration;");
 
         return 0;
     }
